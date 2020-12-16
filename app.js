72379933c8643d9732d35602d8201f978f03c1f0
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2020-12-15 18:41:48 -0800

    [posts.js] New posts now save to our database in mongo

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const express = require('express')
-
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json());
 
 
 // Connect to database
--- controllers/posts.js
+++ controllers/posts.js
@@ -1,15 +1,24 @@
 const express = require('express');
-
 const router = express.Router();
+const Post = require('../models/Post');
 
 
 router.get('/', (req, res) => {
     res.send('We are on posts through the router')
 });
 
-
-router.get('/:id', (req, res) => {
-    res.send('Specific post')
-});
+router.post('/', (req, res) => {
+    const posts = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });
+    posts.save()
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => {
+        res.json({message: err})
+    });
+})
 
 module.exports = router;
